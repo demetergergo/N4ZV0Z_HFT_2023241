83e8676f0571d3b624f18041c2cc23847a89339d
demetergergo6@stud.uni-obuda.hu <demetergergo6@stud.uni-obuda.hu>
2023-11-24 22:36:03 +0100

    "Update()" updated for all repositories to check for virtual properties

--- N4ZV0Z_HFT_2023241.Repository/ModelRepositories/EmployeeRepository.cs
+++ N4ZV0Z_HFT_2023241.Repository/ModelRepositories/EmployeeRepository.cs
@@ -24,7 +24,10 @@ namespace N4ZV0Z_HFT_2023241.Repository
             var old = Read(item.EmployeeId);
             foreach (var prop in old.GetType().GetProperties())
             {
-                prop.SetValue(old, prop.GetValue(item));
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
             }
             ctx.SaveChanges();
         }
--- N4ZV0Z_HFT_2023241.Repository/ModelRepositories/GameRepository.cs
+++ N4ZV0Z_HFT_2023241.Repository/ModelRepositories/GameRepository.cs
@@ -23,7 +23,10 @@ namespace N4ZV0Z_HFT_2023241.Repository
             var old = Read(item.GameID);
             foreach (var prop in old.GetType().GetProperties())
             {
-                prop.SetValue(old, prop.GetValue(item));
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
             }
             ctx.SaveChanges();
         }
