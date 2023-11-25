062c2faf009918a63008eb2c1629b8688c8ee7f5
demetergergo6@stud.uni-obuda.hu <demetergergo6@stud.uni-obuda.hu>
2023-11-25 21:53:29 +0100

    Non CRUD methods added to StatisticsController

---
+++ N4ZV0Z_HFT_2023241.Endpoint/Controllers/StatisticsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using N4ZV0Z_HFT_2023241.Logic;
+using System.Collections.Generic;
+using System.Linq;
+using static N4ZV0Z_HFT_2023241.Logic.GameLogic;
+
+namespace MovieDbApp.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        IGameLogic logic;
+
+        public StatController(IGameLogic logic)
+        {
+            this.logic = logic;
+        }
+        [HttpGet]
+        public IEnumerable<MostIncomeGamePerPublisherInfo> MostIncomeGamePerPublisher()
+        {
+            return this.logic.MostIncomeGamePerPublisher();
+        }
+
+        [HttpGet]
+        public IEnumerable<DevelopersCountAtGamesInfo> DevelopersCountAtGames()
+        {
+            return this.logic.DevelopersCountAtGames();
+        }
+        [HttpGet]
+        public IEnumerable<PublishersByAverageRatingInfo> PublishersByAverageRating()
+        {
+            return this.logic.PublishersByAverageRating();
+        }
+        [HttpGet]
+        public IEnumerable<RatingIncomeRatioPublisherinfo> RatingIncomeRatioPublisher()
+        {
+            return this.logic.RatingIncomeRatioPublisher();
+        }
+
+
+    }
+}
+
