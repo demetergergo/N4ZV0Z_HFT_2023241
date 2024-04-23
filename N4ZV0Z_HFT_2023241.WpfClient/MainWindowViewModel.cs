2ffd41f31032426eedae10e4c19eaf71e164e071
demetergergo6@stud.uni-obuda.hu <demetergergo6@stud.uni-obuda.hu>
2024-04-23 22:53:12 +0200

    Added ui for game CRUD

---
+++ N4ZV0Z_HFT_2023241.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using N4ZV0Z_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace N4ZV0Z_HFT_2023241.WpfClient
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        public RestCollection<Game> Games { get; set; }
+
+        private Game selectedGame;
+
+        public Game SelectedGame
+        {
+            get { return selectedGame; }
+            set 
+            {
+                if (value != null)
+                {
+                    selectedGame = new Game()
+                    {
+                        Title = value.Title,
+                        GameID = value.GameID
+                    };
+                }
+                OnPropertyChanged();
+                (DeleteGameCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        } 
+
+
+
+        public ICommand CreateGameCommand { get; set; } 
+        public ICommand DeleteGameCommand { get; set; } 
+        public ICommand UpdateGameCommand { get; set; } 
+
+        public MainWindowViewModel()
+        {
+            Games = new RestCollection<Game>("http://localhost:35916/", "game");
+            CreateGameCommand = new RelayCommand(() =>
+            {
+                Games.Add(new Game()
+                {
+                    Title = SelectedGame.Title
+                }); 
+            });
+
+            UpdateGameCommand = new RelayCommand(() => 
+            {
+                Games.Update(selectedGame);
+            });
+
+            DeleteGameCommand = new RelayCommand(() =>
+            {
+                Games.Delete(SelectedGame.GameID);
+            }, 
+            () =>
+            {
+                return SelectedGame != null;
+            });
+            selectedGame = new Game();
+        }
+    }
+}
