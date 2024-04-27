fceeb550e699cebeec7f3393ac0b868722eae049
demetergergo6 <demetergergo6@gmail.com>
2024-04-27 13:19:19 +0200

    PublisherWindow, PublisherWindowViewModel added

--- N4ZV0Z_HFT_2023241.WpfClient/MainWindowViewModel.cs
+++ N4ZV0Z_HFT_2023241.WpfClient/MainWindowViewModel.cs
@@ -26,6 +26,7 @@ namespace N4ZV0Z_HFT_2023241.WpfClient
 
             PublisherlistListCommand = new RelayCommand(() =>
             {
+                new PublisherWindow().ShowDialog();
             });
 
             EmployeeListCommand = new RelayCommand(() =>
