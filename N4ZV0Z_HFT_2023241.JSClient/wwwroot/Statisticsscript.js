d28c970125db6719460bd5420c001cff08bdb5c0
demetergergo6 <demetergergo6@gmail.com>
2024-05-12 16:39:58 +0200

    added developerscountatgames crud to jsclient

---
+++ N4ZV0Z_HFT_2023241.JSClient/wwwroot/Statisticsscript.js
@@ -0,0 +1,27 @@
+﻿async function getStatistics() {
+    await getDevelopersCountAtGames()
+}
+getDevelopersCountAtGames()
+
+let DeveloperCountAtGamesStat = []
+
+async function getDevelopersCountAtGames() {
+    await fetch('http://localhost:35916/stat/developerscountatgames')
+        .then(x => x.json())
+        .then(y => {
+            DeveloperCountAtGamesStat = y
+            console.log(DeveloperCountAtGamesStat)
+            displayDevelopersCountAtGames()
+        })
+}
+
+function displayDevelopersCountAtGames() {
+    document.querySelector('#resultcrud1').innerHTML = ""
+    DeveloperCountAtGamesStat.forEach(t => {
+        document.querySelector('#resultcrud1').innerHTML +=
+            `<tr>
+          <td>${t.gameTitle}</td>
+          <td>${t.developerCount}</td>
+          </tr>`
+    })
+}
